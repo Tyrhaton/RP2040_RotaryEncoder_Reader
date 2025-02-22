a46aede2a56819edc92af1971338f1700f5a71ce
tyrhaton <tyrhaton@gmail.com>
2025-02-22 22:48:45 +0100

    Refactor rotary encoder implementation for improved value tracking and callback handling

--- rot.c
+++ rot.c
@@ -5,11 +5,6 @@
 static gpio_encoder_mapping_t gpio_encoder_map[MAX_MAPPINGS];
 static int mapping_count = 0;
 
-uint32_t current_time_rotary()
-{
-    return (uint32_t)(time_us_32() / 100000);
-}
-
 void add_gpio_encoder_mapping(uint gpio_pin, rotary_encoder_t encoder)
 {
     if (mapping_count < MAX_MAPPINGS)
@@ -36,189 +31,72 @@ rotary_encoder_t *find_encoder_by_gpio(uint gpio_pin)
     return &gpio_encoder_map[0].encoder;
 }
 
-int a = 0;
-int b = 0;
-int c = 0;
-void encoder_isr(uint gpio, uint32_t events)
+int ccw_cw = 1;
+volatile int Last_Rotary_Value = 0;
+void encoder_callback(uint gpio, uint32_t events)
 {
-
-    // print(uart1, "[-] ISR\n\r");
-
     uint32_t save = save_and_disable_interrupts();
+
+    uint32_t gpio_state = (gpio_get_all() >> 10) & 0b0111; // get all GPIO then mask out bits 10, 11, 12
+    uint8_t enc_value = gpio_state & 0x03;                 // use only bits 0 and 1 for A and B
     rotary_encoder_t *data = find_encoder_by_gpio(gpio);
-    uint32_t current_us = current_time_rotary();
-    uint32_t last_isr_time = data->last_isr_time;
-    data->last_isr_time = current_us;
 
-    if (current_us == last_isr_time)
+    // Static flags for detecting rising edges
+    static bool ccw_fall = false;
+    static bool cw_fall = false;
+
+    if (gpio == data->gpio_channel_a)
     {
-        restore_interrupts_from_disabled(save);
-
-        return;
-    }
-
-    uint8_t current_a = gpio_get(data->gpio_channel_a);
-    uint8_t current_b = gpio_get(data->gpio_channel_b);
-    // print(uart1, "[+] a: %d b:%d\n\r", current_a, current_b);
-
-    // sleep_us(10);
-
-    // uint8_t current_a2 = gpio_get(data->gpio_channel_a);
-    // uint8_t current_b2 = gpio_get(data->gpio_channel_b);
-
-    // // current_a = current_a2;
-    // // current_b = current_b2;
-
-    // print(uart1, "[+] a: %d:%d:%d\n\r", data->last_state_a, current_a, current_a2);
-    // print(uart1, "[+] b: %d:%d:%d\n\r", data->last_state_b, current_b, current_b2);
-
-    // // print(uart1, "[+] a: %d:%d\n\r", current_a, current_a2);
-    // // print(uart1, "[+] b: %d:%d\n\r", current_b, current_b2);
-
-    // if (current_a != current_a2 || current_b != current_b2)
-    // {
-    //     print(uart1, "[-] Bad\n\r");
-    // }
-    int a_total = 0;
-    int b_total = 0;
-
-    int amt = 10;
-    for (int i = 0; i < amt; i++)
-    {
-        sleep_us(1);
-
-        a_total += gpio_get(data->gpio_channel_a);
-        b_total += gpio_get(data->gpio_channel_b);
-    }
-    if (a_total > 1){
-        current_a = 1;
-    }
-    if (b_total > 1){
-        current_b = 1;
-    }
-    // print(uart1, "[+] a: %d b:%d\n\r", a_total, b_total);
-
-    if (current_a == 1 && a_total == 0)
-    {
-        restore_interrupts_from_disabled(save);
-
-        return;
-    }
-    if (current_a == 0 && a_total > 0)
-    {
-        restore_interrupts_from_disabled(save);
-
-        return;
-    }
-    ////////////////
-
-    // if (current_a == 0 && a_total == 0 && current_b == 1 && b_total == 10)
-    // {
-    //     restore_interrupts_from_disabled(save);
-
-    //     return;
-    // }
-
-    // if (current_a == 1 && a_total == 10 && current_b == 1 && b_total == 10)
-    // {
-    //     restore_interrupts_from_disabled(save);
-
-    //     return;
-    // }
-
-    // {
-    //     sleep_us(10);
-
-    //     uint8_t current_a3 = gpio_get(data->gpio_channel_a);
-    //     uint8_t current_b3 = gpio_get(data->gpio_channel_b);
-
-    //     // print(uart1, "[+] a: %d:%d:%d:%d\n\r", data->last_state_a, current_a, current_a2, current_a3);
-    //     // print(uart1, "[+] b: %d:%d:%d:%d\n\r", data->last_state_b, current_b, current_b2, current_b3);
-
-    //     print(uart1, "[+] a: %d:%d:%d\n\r", current_a, current_a2, current_a3);
-    //     print(uart1, "[+] b: %d:%d:%d\n\r", current_b, current_b2, current_b3);
-
-    //     current_a = current_a3;
-    //     current_b = current_b3;
-    // }
-
-    // if ((current_a != data->last_state_a) || (current_b != data->last_state_b))
-    // {
-
-    // if (current_a != data->last_state_a && current_a == 1)
-    // {
-    //     if (current_b != current_a)
-    //     {
-    //         data->dir = 1;
-    //         data->current_value += 1 * data->factor; // CW
-    //     }
-    //     else
-    //     {
-    //         data->dir = -1;
-
-    //         data->current_value += -1 * data->factor; // CCW
-    //     }
-    // }
-    data->last_state_a = current_a;
-    data->last_state_b = current_b;
-
-    if (current_a == current_b)
-    {
-        data->dir = 1;
-        data->current_value += 1 * data->factor; // CW
-    }
-    else
-    {
-        data->dir = -1;
-
-        data->current_value += -1 * data->factor; // CCW
-    }
-
-    // data->current_value += data->dir;
-
-    switch (data->type)
-    {
-    case 1:
-        if (data->current_value < data->min_value)
+        if (enc_value == 0b10) // Channel A rising edge indicates potential CW movement.
         {
-            data->current_value = data->max_value;
+            cw_fall = true;
+            ccw_fall = false; // Clear CCW flag to avoid conflicting triggers.
         }
-        if (data->current_value > data->max_value)
+        else if (enc_value == 0b00 && ccw_fall) // If CCW flag is set and we see 00, complete a CCW cycle.
         {
-            data->current_value = data->min_value;
-        }
-        break;
-    default:
+            cw_fall = false;
+            ccw_fall = false;
 
-        if (data->current_value < data->min_value)
-        {
-            data->current_value = data->min_value;
+            if (ccw_cw == 1)
+            {
+                print(uart1, "switch %d\n\r", Last_Rotary_Value);
+            }
+            Last_Rotary_Value -= 1 * data->factor;
+            if (Last_Rotary_Value < 0)
+            {
+                Last_Rotary_Value = 5;
+            }
+            print(uart1, "CCW %d:\n\r", Last_Rotary_Value);
+            ccw_cw = 0;
         }
-        if (data->current_value > data->max_value)
-        {
-            data->current_value = data->max_value;
-        }
-        break;
     }
-    print(uart1, "[-] => Encdr: %d\n\r", data->current_value);
-
-    if (a == data->current_value)
+    else if (gpio == data->gpio_channel_b)
     {
-        print(uart1, "[+] Value Rot: %d\n\r", data->current_value);
-        print(uart1, "[+] a: %d b:%d\n\r", current_a, current_b);
+        if (enc_value == 0b01) // Channel B rising edge indicates potential CCW movement.
+        {
+            ccw_fall = true;
+            cw_fall = false; // Clear CW flag to avoid conflicting triggers.
+        }
+        else if (enc_value == 0b00 && cw_fall) // If CW flag is set and we see 00, complete a CW cycle.
+        {
+            cw_fall = false;
+            ccw_fall = false;
+            if (ccw_cw == 0)
+            {
+                print(uart1, "switch %d\n\r", Last_Rotary_Value);
+            }
+            Last_Rotary_Value += 1 * data->factor;
+            if (Last_Rotary_Value > 5)
+            {
+                Last_Rotary_Value = 0;
+            }
+            print(uart1, "CW  %d:\n\r", Last_Rotary_Value);
+            // print(uart1, "%d\n\r", data->dir);
 
-        print(uart1, "[+] a: %d b:%d\n\r", a_total, b_total);
+            // data->dir = 1;
+            ccw_cw = 1;
+        }
     }
-    a = b;
-    b = data->current_value;
-
-    // }
-    // else
-    // {
-    //     print(uart1, "[-] Bad\n\r");
-    //     print(uart1, "[-] dir: %d\n\r", data->dir);
-    // }
-
     restore_interrupts_from_disabled(save);
 }
 
@@ -237,20 +115,21 @@ rotary_encoder_t create_rotary(int gpio_channel_a, int gpio_channel_b, int min_v
     encoder.last_state_b = 0;
     encoder.type = type;
     encoder.dir = 1;
+    encoder.ccw_cw = 0;
 
     add_gpio_encoder_mapping(gpio_channel_a, encoder);
+    add_gpio_encoder_mapping(gpio_channel_b, encoder);
 
     gpio_init(gpio_channel_a);
     gpio_set_dir(gpio_channel_a, GPIO_IN);
-    gpio_pull_up(gpio_channel_a);
-    add_gpio_encoder_mapping(gpio_channel_a, encoder);
-    gpio_set_irq_enabled_with_callback(gpio_channel_a, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &encoder_isr);
+    gpio_disable_pulls(gpio_channel_a);
 
     gpio_init(gpio_channel_b);
     gpio_set_dir(gpio_channel_b, GPIO_IN);
-    gpio_pull_up(gpio_channel_b);
+    gpio_disable_pulls(gpio_channel_b);
 
-    irq_set_enabled(IO_IRQ_BANK0, true);
+    gpio_set_irq_enabled_with_callback(gpio_channel_a, GPIO_IRQ_EDGE_FALL, true, &encoder_callback);
+    gpio_set_irq_enabled(gpio_channel_b, GPIO_IRQ_EDGE_FALL, true);
 
     return encoder;
 }
